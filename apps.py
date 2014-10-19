45f52d6ec080299db138a9dcc4334dbdfeb8d322
Stanislav Lobanov <n10101010@gmail.com>
2014-10-19 12:38:13 +0400

    Added default application config

---
+++ apps.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from __future__ import print_function, unicode_literals
+
+from django.apps import AppConfig
+
+
+class DefaultConfig(AppConfig):
+    name = '{{ app_name }}'
+    verbose_name = '{{ app_name }}'
+
+    def ready(self):
+        # импортировать сигналы для их регистрации
+        import {{ app_name }}.signals
