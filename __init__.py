961f9392efabffe8b246651bf6274a680351dc87
Stanislav Lobanov <n10101010@gmail.com>
2014-12-01 17:57:50 +0300

    Added division future import to all python files

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # coding: utf-8
 
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, division
 
 default_app_config = '{{ app_name }}.apps.DefaultConfig'
--- views.py
+++ views.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, division
 
 from django.shortcuts import render
+
+from {{ app_name }} import queries
 
 
