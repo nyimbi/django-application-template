33c55ef1b7e4bf4a8fabd2ca9960c369f43171e4
Stanislav Lobanov <n10101010@gmail.com>
2014-12-01 17:57:50 +0300

    Added division future import to all python files

--- managers.py
+++ managers.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, division
 
 from django.db.models import Manager, Q
 from django.db.models.query import QuerySet
