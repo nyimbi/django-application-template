7687b03f433f4bbbd439ef05af0247a0c3a3b47b
Stanislav Lobanov <n10101010@gmail.com>
2015-07-20 14:24:59 +0300

    Removed post-migrage hook

--- apps.py
+++ apps.py
@@ -3,9 +3,6 @@
 from __future__ import print_function, unicode_literals, division
 
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
-from {{ app_name }}.post_migrate import post_migrate_{{ app_name }}
 
 
 class DefaultConfig(AppConfig):
@@ -15,4 +12,3 @@ class DefaultConfig(AppConfig):
     def ready(self):
         # импортировать сигналы для их регистрации
         import {{ app_name }}.signals
-        post_migrate.connect(post_migrate_{{ app_name }}, sender=self)
